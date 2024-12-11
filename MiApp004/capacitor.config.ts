24dd7d48e9d275b470d85f24eb36a949934a6611
Felipe Novoa Manríquez <felipenmq@gmail.com>
2024-12-10 23:06:08 -0300

    lel

--- MiApp004/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'MiApp004',
-  webDir: 'www'
-};
-
-export default config;
